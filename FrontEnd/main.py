2a146f105f6272954f733c9a00b545e6b57f88b2
Oscar Song <Oscar.Song03@gmail.com>
2022-01-03 21:52:21 -0800

    new changes

---
+++ FrontEnd/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from FrontEnd/finalChat import chat
+
+app = Flask(__name__, template_folder="Template")
+
+# This is the get request when the user inputs information
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# This is the post request when the user clicks the chat button
+@app.route('/get')
+def post_bot_response(userText):
+    userText = request.get_json.get('message')
+    return chat(userText)
+    response = chat(userText)
+    message = {'message': response}
+    return jsonify(message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
