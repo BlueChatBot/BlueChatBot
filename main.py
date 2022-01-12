5a52a31ae7a48b05113dd6773bb4edc2e5c851bc
Areeb <areebkhan280@gmail.com>
2022-01-12 00:37:07 -0500

    II FIGURED IT OUT

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 # import sys
 # sys.path.append('/Users/oscarsong/Desktop/BlueChatBot/BotLogic')
 from finalChat import chat
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, template_folder='static/Templates')
+CORS(app)
 
 # This is the get request when the user inputs information
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
 
 # This is the post request when the user clicks the chat button
-@app.route('/get')
-def post_bot_response(userText):
-    userText = request.get_json.get('message')
-    return chat(userText)
-    response = chat(userText)
-    message = {'message': response}
+@app.post('/predict')
+def post_bot_response():
+    sentence = request.get_json().get("message")
+    response = chat(sentence)
+    message = {"answer": response}
     return jsonify(message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
