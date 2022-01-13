942c2e40146992ded2af3bc615dc2b73d164e32f
osong <osong@umich.edu>
2022-01-13 16:47:47 -0500

    x

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html')
 
 # This is the post request when the user clicks the chat button
-@app.route('/msg', methods=['POST'])
+@app.route('/post/', methods=['POST'])
 def post_bot_response():
     sentence = request.get_json().get("message")
     response = chat(sentence)
