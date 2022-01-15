718a254c89b709999f21376ee01f292bc3741e99
osong <osong@umich.edu>
2022-01-15 17:51:37 -0500

    x

--- static/Styles/interpreter.js
+++ static/Styles/interpreter.js
@@ -45,7 +45,7 @@ class BlueChatBot {
         this.textMessage.push(msg1);
         // fetch from database
         // MODIFIED FOR HEROKU
-        fetch('https://blue-chatbot.herokuapp.com/post/', {
+        fetch('/post/', {
             method: 'POST',
             body: JSON.stringify({ message: message1 }),
             mode: 'cors',
